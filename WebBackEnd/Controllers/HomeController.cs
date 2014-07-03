40942523fff5d45e3c2aca0ebf60f5e5ee4187c7
Panayotis Trapatsas <trapatsas@gmail.com>
2014-07-03 16:32:04 +0300

    TODO: Logout Button, Validation Error Handling, Show username/success msg

---
+++ WebBackEnd/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBackEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
